59eb21e275906082c985dff000eb0de684f0a8ca
rajupucha <puchagingalaraju@gmail.com>
2025-04-26 18:35:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openPopup() {
+    document.getElementById('popup').style.display = 'flex';
+  }
+  
+  function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+  }
+  
